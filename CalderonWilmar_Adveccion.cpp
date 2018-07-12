2c98c74c8bb9229f09901425ad58f2dc781dd77b
wilmarc <wa.calderon@uniandes.edu.co>
2018-07-12 12:14:26 -0500

    Intento de mejora

--- CalderonWilmar_Adveccion.cpp
+++ CalderonWilmar_Adveccion.cpp
@@ -21,22 +21,23 @@ for (int k=1;k<nx;k++){
 		for (int j=0;j<nx;j++){
 		upre[0]=1.0;
 		
-		if (i==0){
+		//if (i==0){
 			if(x[j]>=0.75 && x[j]<=1.25){
 				upre[j]=2.0;
 			}		
 			else{
 				upre[j]=1.0;
 			}
-			}
-		else{
+			//ufut[j]=upre[j];
+		//	}
+		//else{
 			ufut[j]=(-1.0*c)*(dt/dx)*(upre[j]-upre[j-1])+upre[j];
-		}
+		//}
 
 		}
 		for (int j=0;j<nx;j++){
 		upre[j]=ufut[j];		
-		cout << j <<" " << upre[j] <<endl;
+		cout << x[j] <<" " << upre[j] <<endl;
 		}
 }
 return 0;
--- CalderonWilmar_plots.py
+++ CalderonWilmar_plots.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 d=np.genfromtxt("plot.txt", delimiter=" ")
-x,y = d[:,0],d[:,1]
+x,y = d[0:80,0],d[0:80,1]
 
 plt.figure()
 plt.plot(x,y)
